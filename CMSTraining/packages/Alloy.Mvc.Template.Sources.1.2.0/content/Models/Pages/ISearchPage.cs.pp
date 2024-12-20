97b82ed829f4f6fd99b80c294cbbb24035f1ec24
Colin Thompson <thompsoncolinm@gmail.com>
2024-12-20 11:47:21 -0500

    added alloy training site

--- CMSTraining/packages/Alloy.Mvc.Template.Sources.1.2.0/content/Models/Pages/ISearchPage.cs.pp
+++
@@ -1,9 +0,0 @@
-namespace $rootnamespace$.Models.Pages
-{
-    /// <summary>
-    /// Marker interface for search implementation
-    /// </summary>
-    public interface ISearchPage
-    {
-    }
-}
